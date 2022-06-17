03e6206b93727a1d377bb0c4cf699dd4d56f66f2
Bodan1992 <bodan1992@gmail.com>
2022-06-18 01:01:40 +0300

    fix HW2 + add fixture to conftest

--- Automation_google2.py
+++ Automation_google2.py
@@ -9,7 +9,6 @@ def test_search_valid(url):
     browser.element('[name="q"]').should(be.blank).type('Selene - Web UI browser in Python').press_enter()
     browser.element('[data-test-id="mainline"]').should(have.text("User-oriented Web UI browser tests in Python - GitHub"))
 
-
 def test_search_invalid(url):
     browser.element('[name="q"]').should(be.blank).type('SeSeSeSeSeSeSeSeSeSeSeSeSeSeSeSeSeSeSeSadasdsad5q4w4qww4eSeSeSeSeSeSeSeSeSev').press_enter()
     browser.element('[data-test-id="message-tips-message"]').should(have.text("Yikes! We didn't find any results for “SeSeSeSeSeSeSeSeSeSeSeSeSeSeSeSeSeSeSeSadasdsad5q4w4qww4eSeSeSeSeSeSeSeSeSev”"))
