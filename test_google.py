cf9f23db2663988f08db3d4716ac0b8f65f9ded0
Bodan1992 <bodan1992@gmail.com>
2022-06-21 14:24:31 +0300

    add part test google

---
+++ test_google.py
@@ -0,0 +1,38 @@
+
+import pytest
+
+@pytest.fixture()                                      # decorator(декоратор)
+def before_each(request):
+    print("Called before each test " + request.node.name)               # request.node.name-проверка правда ли запускается фикстура перед каждым тестом
+
+@pytest.fixture(scope='session',autouse=True)                                      # decorator(декоратор)/ scope='session-вызывается для каждой сессии
+def init_browser(request):                                                         #autouse=True-неявная инициализация(запуск в каждом тесте)
+    print("Called before all tests " + request.node.name)
+
+@pytest.fixture()
+def message():
+    return "This is message"
+
+
+@pytest.fixture()
+def client():
+    print(123)
+    yield                                           #запус
+    print("А теперь удаляем клиента")
+
+
+def test_first(before_each):
+    assert 1 == 1
+
+def test_second(before_each):
+    assert 1 == 2, "Единица не должна быть равна двойке!"
+
+
+def test_message(message,firefox):
+    print(message)
+    assert "message" in message
+
+def test_client(client):
+    assert client == 123
+
+
